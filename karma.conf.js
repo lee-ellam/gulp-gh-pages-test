e1d5095a187185f7c0e70fab6963f91200ee8beb
Lee Ellam <lee.ellam@justgiving.com>
2015-10-07 15:41:11 +0100

    add stuff

---
+++ karma.conf.js
@@ -0,0 +1,34 @@
+module.exports = function(config) {
+
+	config.set({
+
+		basePath: '',
+
+		browsers: ['PhantomJS'],
+
+		frameworks: ['browserify', 'jasmine'],
+
+		files: [
+			'test/**/*.js'
+		],
+
+		exclude: [
+		],
+
+		preprocessors: {
+			'test/**/*.js': ['browserify']
+		},
+
+		browserify: {
+			debug: true,
+			transform: ['babelify'],
+			paths: [
+				'./node_modules',
+				'./bower_components/'
+			]
+		},
+
+		reporters: ['mocha']
+	});
+
+};
